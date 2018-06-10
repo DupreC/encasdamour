1ff9f94eecd434076caa295388d72b8dd2475c3d
colin <dupre.col@gmail.com>
2018-06-10 22:54:08 +0200

    redirection

---
+++ pages/page_experience.php
@@ -0,0 +1,10 @@
+<?php
+include '../header.php';
+?>
+<?php
+include_once "../functions/php/main.php";
+   define('NOM_SITE', 'accueil', true);
+   get_fil_ariane(array('final' => 'Ambiance'));
+?>
+<?php
+include '../footer.php';
