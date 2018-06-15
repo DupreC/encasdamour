ae466dbc1e4b12fca6f730fd575d6bdb2f36442d
Installation <dupre.col@gmail.com>
2018-06-15 17:03:00 +0200

    refont home

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
     <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
     <script type="text/javascript" src="functions/js/main.js"></script>
     <title>Encas d'amour</title>
@@ -16,17 +17,17 @@
 <body class="container">
 <header class="row">
     <ul class="col-md-8 flex nav">
-        <li><h1><img src="img/logo.png" alt="encasdamour"></h1></li>
+        <li><h1><img src="img/logo_rose1.png" alt="encasdamour"></h1></li>
         <li><a href="#">Comment ça marche ?</a></li>
         <li><a href="#">Les ambiances</a></li>
         <li><a href="#">L’équipe</a></li>
-        <li><a class="button-simple" href="choix-ambiances">Tentez l’expérience</a></li>
+<!--        <li><a class="button-simple" href="choix-ambiances">Tentez l’expérience</a></li>-->
     </ul>
-    <ul class="col-md-3 right flex rs">
+    <ul class="col-md-2 right flex rs">
         <li><p>Suivez-nous</p></li>
-        <li><a href="#"><img src="img/facebook.png" alt="facebook"></a></li>
-        <li><a href="#"><img src="img/instagram.png" alt="instagram"></a></li>
-        <li><a href="#"><img src="img/twitter.png" alt="twitter"></a></li>
+        <li><a href="https://www.facebook.com/encasdamour"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="https://twitter.com/Encasdamour"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="https://www.instagram.com/encas_damour/"><i class="fab fa-instagram"></i></a></li>
     </ul>
 </header>
 
@@ -35,48 +36,67 @@
         <div class="col-md-10 right no-padding">
             <h2>On vous aide à organiser le plus romantique des dîners</h2>
             <p class="col-md-11">Vivez une expérience romantique unique et féérique où vous voulez et quand vous voulez grâce à un service de livraison complet.</p>
-            <form class="clear row" action="#">
-                <input type="text" placeholder="Saississez votre adresse">
-                <input type="submit" value="O">
-            </form>
+<!--            <li><a class="button-simple" href="choix-ambiances">Tentez l’expérience</a></li>-->
         </div>
+    </div>
+    <div class="col-md-1 cercle">
+        <img src="img/cercle_1.png" alt="cercle_1">
+        <img src="img/cercle_2.png" alt="cercle_2">
+    </div>
+    <div class="col-md-5 img_header">
+        <img src="img/img_header.png" alt="img_header">
     </div>
 </div>
 <div class="bloc_explication_home row">
+    <svg class="background" width="428" height="559" viewBox="0 0 428 559" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 464.982C53.1252 514.842 98.9122 537.986 99.4257 538.244C107.431 542.6 115.84 546.376 124.655 549.572C196.828 574.74 275.141 547.927 320.982 488.129C320.982 488.129 397.95 394.832 425.356 211.281C439.824 126.326 393.874 42.0283 313.2 13.7102C304.386 10.5144 295.435 8.25705 286.392 6.62545C286.392 6.62545 149.56 -22.407 0 41.0761V464.982Z" fill="#B9C7F6"/>
+    </svg>
+
     <h3>Nos petits doigts de fée à votre service<br><strong>Comment ça marche ?</strong></h3>
-    <ul class="row ">
-        <li class="col-md-2 col-md-offset-2"><img src="" alt="ambiance">
+    <ul class="row">
+        <li class="col-md-2 col-md-offset-2">
+            <img src="img/picto_ambiance.png" alt="ambiance">
             <h5>Une ambiance</h5>
             <p>Choisissez l'ambiance que vous souhaitez donner à votre dîner pour le rendre unique</p>
         </li>
-        <li class="col-md-2 col-md-offset-1"><img src="" alt="menu">
+        <li class="col-md-2 col-md-offset-1">
+            <img src="img/picto_menu.png" alt="menu">
             <h5>Un menu</h5>
             <p>Choissiez le menu qui vous convient selon vos goûts et celles de votre partenaires.</p>
         </li>
-        <li class="col-md-2 col-md-offset-1"><img src="" alt="date">
+        <li class="col-md-2 col-md-offset-1">
+            <img src="img/picto_calendar.png" alt="date">
             <h5>Une date</h5>
             <p>Dite-nous quand et à quelle heure aura lieu votre diner. Nous vous livrerons quelques heures avant. </p>
         </li>
     </ul>
+    <div class="ellipse">
+        <img src="img/Ellipse.png" alt="Ellipse">
+        <img src="img/Ellipse2.png" alt="Ellipse2">
+    </div>
 </div>
 <div class="bloc_explication_home bloc_choix-ambiances_home row">
+    <img class="background" src="img/Subtract.png" alt="">
     <h3>Pour une soirée maqigue <br><strong>Les ambiances</strong></h3>
     <p>Toutes les ambiances comprennent un menu complet et les éléments de décorations associées </p>
     <ul class="row col-md-10">
-        <li class="col-md-4"><img src="" alt="ambiance">
+        <li class="col-md-4">
+            <img src="img/img_ieuf.png" alt="ambiance">
             <h5>Royale</h5>
             <p>Un ambiance digne d’un conte de fée.</p>
-            <a class="button-simple" href="#">Choisir cette ambiance</a>
+<!--            <a class="button-simple" href="#">Choisir cette ambiance</a>-->
         </li>
-        <li class="col-md-4"><img src="" alt="menu">
+        <li class="col-md-4">
+            <img src="img/img_be.png" alt="belle_étoile">
             <h5>Urbaine/panorama</h5>
             <p>Les couleurs et la lumière de la ville</p>
-            <a class="button-simple" href="#">Choisir cette ambiance</a>
+<!--            <a class="button-simple" href="#">Choisir cette ambiance</a>-->
         </li>
-        <li class="col-md-4"><img src="" alt="date">
+        <li class="col-md-4">
+            <img src="img/img_cp.png" alt="date">
             <h5>Nature/forêt</h5>
             <p>Respirez un bonne bouffée d'air frais</p>
-            <a class="button-simple" href="#">Choisir cette ambiance</a>
+<!--            <a class="button-simple" href="#">Choisir cette ambiance</a>-->
         </li>
     </ul>
 </div>
